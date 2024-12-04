cba698f4064266ff72d9abf5638c4e0bb2051999
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2024-12-04 14:59:16 +0530

    local storage and navigate

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,6 +8,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App className="bg-black" />
+    <App />
   </StrictMode>,
 )
